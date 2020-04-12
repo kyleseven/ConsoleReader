1b6770ae9f389967af52d531fabe0829a1993af7
kyleseven <kyle.senerpida@icloud.com>
2020-04-12 14:04:28 -0500

    Format message like console.

--- src/main/java/me/kyleseven/consolereader/logreader/LogAppender.java
+++ src/main/java/me/kyleseven/consolereader/logreader/LogAppender.java
@@ -6,13 +6,18 @@ import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.bukkit.entity.Player;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class LogAppender extends AbstractAppender {
 
     private final Player player;
+    private final SimpleDateFormat dateFormatter;
 
     public LogAppender(Player player) {
         super("LogReader", null, null, false, null);
         this.player = player;
+        dateFormatter = new SimpleDateFormat("HH:mm:ss");
         start();
     }
 
@@ -20,6 +25,7 @@ public class LogAppender extends AbstractAppender {
     public void append(LogEvent event) {
         LogEvent log = event.toImmutable();
         String message = log.getMessage().getFormattedMessage();
+        message = "[" + dateFormatter.format(new Date(log.getTimeMillis()) + log.getLevel().toString() + "] " + message);
         Utils.sendMsg(player, MainConfig.getInstance().getLogColor() + message);
     }
 }
