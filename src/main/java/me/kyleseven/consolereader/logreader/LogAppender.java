a99232e35e0310b05a8909d71cab3585a6c7a523
kyleseven <kyle.senerpida@icloud.com>
2020-04-12 14:31:51 -0500

    fixup! Format message like console.

--- src/main/java/me/kyleseven/consolereader/logreader/LogAppender.java
+++ src/main/java/me/kyleseven/consolereader/logreader/LogAppender.java
@@ -2,22 +2,18 @@ package me.kyleseven.consolereader.logreader;
 
 import me.kyleseven.consolereader.Utils;
 import me.kyleseven.consolereader.config.MainConfig;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.bukkit.entity.Player;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class LogAppender extends AbstractAppender {
 
     private final Player player;
-    private final SimpleDateFormat dateFormatter;
 
     public LogAppender(Player player) {
-        super("LogReader", null, null, false, null);
+        super("LogReader", null, null, false);
         this.player = player;
-        dateFormatter = new SimpleDateFormat("HH:mm:ss");
         start();
     }
 
@@ -25,7 +21,7 @@ public class LogAppender extends AbstractAppender {
     public void append(LogEvent event) {
         LogEvent log = event.toImmutable();
         String message = log.getMessage().getFormattedMessage();
-        message = "[" + dateFormatter.format(new Date(log.getTimeMillis()) + log.getLevel().toString() + "] " + message);
+        message = "[" + DateFormatUtils.format(log.getTimeMillis(), "HH:mm:ss") + " " + log.getLevel().toString() + "]: " + message;
         Utils.sendMsg(player, MainConfig.getInstance().getLogColor() + message);
     }
 }
