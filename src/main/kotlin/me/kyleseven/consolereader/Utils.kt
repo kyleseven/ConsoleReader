3b3cd02b35b3d7655666da605c43438828318b6a
kyleseven <kyle.senerpida@icloud.com>
2020-05-10 15:28:41 -0500

    Rewrite Utils as Kotlin extensions

---
+++ src/main/kotlin/me/kyleseven/consolereader/Utils.kt
@@ -0,0 +1,13 @@
+package me.kyleseven.consolereader
+
+import me.kyleseven.consolereader.config.MainConfig
+import org.bukkit.ChatColor
+import org.bukkit.command.CommandSender
+
+fun CommandSender.sendColorMsg(message: String) {
+    sendMessage(ChatColor.translateAlternateColorCodes('&', message))
+}
+
+fun CommandSender.sendPrefixMsg(message: String) {
+    sendMessage(ChatColor.translateAlternateColorCodes('&', MainConfig.prefix + message))
+}
