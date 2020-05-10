bc5ba0b97d50675ce218eeea6707707b9466cd3f
kyleseven <kyle.senerpida@icloud.com>
2020-05-10 15:35:50 -0500

    Re-add contents of plugin class

--- src/main/kotlin/me/kyleseven/consolereader/ConsoleReader.kt
+++ src/main/kotlin/me/kyleseven/consolereader/ConsoleReader.kt
@@ -1,5 +1,12 @@
 package me.kyleseven.consolereader
 
+import co.aikar.commands.PaperCommandManager
+import me.kyleseven.consolereader.commands.MainCommand
+import me.kyleseven.consolereader.config.MainConfig
+import me.kyleseven.consolereader.listeners.PlayerListener
+import me.kyleseven.consolereader.logreader.LogAppenderManager
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.Logger
 import org.bukkit.plugin.java.JavaPlugin
 
 class ConsoleReader : JavaPlugin() {
@@ -10,22 +17,25 @@ class ConsoleReader : JavaPlugin() {
 
     override fun onEnable() {
         instance = this
-
+        loadConfigs()
+        registerCommands()
+        registerEvents()
+        LogAppenderManager.setup(LogManager.getRootLogger() as Logger)
     }
 
     override fun onDisable() {
-
+        LogAppenderManager.stopReadingAll()
     }
 
     private fun loadConfigs() {
-
+        MainConfig
     }
 
     private fun registerCommands() {
-
+        PaperCommandManager(this).registerCommand(MainCommand())
     }
 
     private fun registerEvents() {
-
+        server.pluginManager.registerEvents(PlayerListener(), this)
     }
 }
