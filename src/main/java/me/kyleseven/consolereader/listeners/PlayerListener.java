d62dde0c58f002895b4ad4d92225973b9b9edd9e
kyleseven <kyle.senerpida@icloud.com>
2020-05-10 13:56:17 -0500

    Capitalize PlayerListener to conform to naming conventions.

--- src/main/java/me/kyleseven/consolereader/listeners/PlayerListener.java
+++ src/main/java/me/kyleseven/consolereader/listeners/PlayerListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public class playerListener implements Listener {
+public class PlayerListener implements Listener {
     @EventHandler
     public void removeReadingPlayer(PlayerQuitEvent e) {
         // Remove the appender if player is offline
