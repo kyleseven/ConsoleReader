3671dd84a671f5b9018dbb209e38a2aac4113c7a
kyleseven <kyle.senerpida@icloud.com>
2020-05-16 00:57:20 -0500

    Move companion object to bottom to conform to conventions.

--- src/main/kotlin/me/kyleseven/consolereader/ConsoleReader.kt
+++ src/main/kotlin/me/kyleseven/consolereader/ConsoleReader.kt
@@ -10,11 +10,6 @@ import org.apache.logging.log4j.core.Logger
 import org.bukkit.plugin.java.JavaPlugin
 
 class ConsoleReader : JavaPlugin() {
-    companion object {
-        var instance: ConsoleReader? = null
-            private set
-    }
-
     override fun onEnable() {
         instance = this
         loadConfigs()
@@ -37,5 +32,10 @@ class ConsoleReader : JavaPlugin() {
 
     private fun registerEvents() {
         server.pluginManager.registerEvents(PlayerListener(), this)
+    }
+
+    companion object {
+        var instance: ConsoleReader? = null
+            private set
     }
 }
