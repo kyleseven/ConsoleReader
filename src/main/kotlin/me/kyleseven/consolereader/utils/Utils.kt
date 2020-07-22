9824c47b6b056b50312cfd6fecce21d50b23201c
kyleseven <kyle.senerpida@icloud.com>
2020-07-22 01:41:42 -0500

    Switch from Bukkit ChatColor to Bungee ChatColor

--- src/main/kotlin/me/kyleseven/consolereader/utils/Utils.kt
+++ src/main/kotlin/me/kyleseven/consolereader/utils/Utils.kt
@@ -1,7 +1,7 @@
 package me.kyleseven.consolereader.utils
 
 import me.kyleseven.consolereader.config.MainConfig
-import org.bukkit.ChatColor
+import net.md_5.bungee.api.ChatColor
 import org.bukkit.command.CommandSender
 
 fun CommandSender.sendColorMsg(message: String) {
@@ -15,27 +15,27 @@ fun CommandSender.sendPrefixMsg(message: String) {
 fun parseANSI(message: String): String {
     var messageToParse = message
     val ansiMap = mapOf(
-        "\u001B[0;32;1m" to net.md_5.bungee.api.ChatColor.GREEN,
-        "\u001B[0;36;1m" to net.md_5.bungee.api.ChatColor.AQUA,
-        "\u001B[0;31;1m" to net.md_5.bungee.api.ChatColor.RED,
-        "\u001B[0;35;1m" to net.md_5.bungee.api.ChatColor.LIGHT_PURPLE,
-        "\u001B[0;33;1m" to net.md_5.bungee.api.ChatColor.YELLOW,
-        "\u001B[0;37;1m" to net.md_5.bungee.api.ChatColor.WHITE,
-        "\u001B[0;30;22m" to net.md_5.bungee.api.ChatColor.BLACK,
-        "\u001B[0;34;22m" to net.md_5.bungee.api.ChatColor.DARK_BLUE,
-        "\u001B[0;32;22m" to net.md_5.bungee.api.ChatColor.DARK_GREEN,
-        "\u001B[0;36;22m" to net.md_5.bungee.api.ChatColor.DARK_AQUA,
-        "\u001B[0;31;22m" to net.md_5.bungee.api.ChatColor.DARK_RED,
-        "\u001B[0;35;22m" to net.md_5.bungee.api.ChatColor.DARK_PURPLE,
-        "\u001B[0;33;22m" to net.md_5.bungee.api.ChatColor.GOLD,
-        "\u001B[0;37;22m" to net.md_5.bungee.api.ChatColor.GRAY,
-        "\u001B[0;30;1m" to net.md_5.bungee.api.ChatColor.DARK_GRAY,
-        "\u001B[5m" to net.md_5.bungee.api.ChatColor.MAGIC,
-        "\u001B[21m" to net.md_5.bungee.api.ChatColor.BOLD,
-        "\u001B[9m" to net.md_5.bungee.api.ChatColor.STRIKETHROUGH,
-        "\u001B[4m" to net.md_5.bungee.api.ChatColor.UNDERLINE,
-        "\u001B[3m" to net.md_5.bungee.api.ChatColor.ITALIC,
-        "\u001B[m" to net.md_5.bungee.api.ChatColor.RESET
+        "\u001B[0;32;1m" to ChatColor.GREEN,
+        "\u001B[0;36;1m" to ChatColor.AQUA,
+        "\u001B[0;31;1m" to ChatColor.RED,
+        "\u001B[0;35;1m" to ChatColor.LIGHT_PURPLE,
+        "\u001B[0;33;1m" to ChatColor.YELLOW,
+        "\u001B[0;37;1m" to ChatColor.WHITE,
+        "\u001B[0;30;22m" to ChatColor.BLACK,
+        "\u001B[0;34;22m" to ChatColor.DARK_BLUE,
+        "\u001B[0;32;22m" to ChatColor.DARK_GREEN,
+        "\u001B[0;36;22m" to ChatColor.DARK_AQUA,
+        "\u001B[0;31;22m" to ChatColor.DARK_RED,
+        "\u001B[0;35;22m" to ChatColor.DARK_PURPLE,
+        "\u001B[0;33;22m" to ChatColor.GOLD,
+        "\u001B[0;37;22m" to ChatColor.GRAY,
+        "\u001B[0;30;1m" to ChatColor.DARK_GRAY,
+        "\u001B[5m" to ChatColor.MAGIC,
+        "\u001B[21m" to ChatColor.BOLD,
+        "\u001B[9m" to ChatColor.STRIKETHROUGH,
+        "\u001B[4m" to ChatColor.UNDERLINE,
+        "\u001B[3m" to ChatColor.ITALIC,
+        "\u001B[m" to ChatColor.RESET
     )
 
     for ((ansi, color) in ansiMap) {
