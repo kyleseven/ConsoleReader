kyleseven/ConsoleReader

commit 479cfc54ad91fc5c86aee052453e184ed6f2b23d

Use ifBlank for conciseness

src/main/kotlin/me/kyleseven/consolereader/commands/MainCommand.kt
@@ -78,7 +78,7 @@ class MainCommand : BaseCommand() {
                     .event(HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText.append("Click to suggest.").color(ChatColor.GRAY).create()))
             }
 
-            helpEntry.append(if (command.args.isEmpty()) "" else " ${command.args}", ComponentBuilder.FormatRetention.NONE).color(ChatColor.AQUA)
+            helpEntry.append(command.args.ifBlank { "" }, ComponentBuilder.FormatRetention.NONE).color(ChatColor.AQUA)
                 .append(" - ").color(ChatColor.DARK_GRAY)
                 .append(command.description).color(ChatColor.GRAY)
 

src/main/kotlin/me/kyleseven/consolereader/logreader/LogAppender.kt
@@ -27,7 +27,7 @@ class LogAppender(private val player: Player) : AbstractAppender("ConsoleReader-
         val logDate = DateFormatUtils.format(log.timeMillis, "yyyy-MM-dd")
         val logTime = DateFormatUtils.format(log.timeMillis, "HH:mm:ss")
         val logLevel = log.level.toString()
-        val loggerName = if (log.loggerName.isEmpty()) "None" else log.loggerName
+        val loggerName = log.loggerName.ifBlank { "None" }
         val threadName = log.threadName
         var messagePrefix = "[$logTime $logLevel]: "
 

commit 8eef21096f729ff6222922729bb6efaf3601d696

Insert space between command and args. (Fixes issue caused by 479cfc54)

src/main/kotlin/me/kyleseven/consolereader/commands/MainCommand.kt
@@ -114,7 +114,7 @@ class MainCommand : BaseCommand() {
                     )
             }
 
-            helpEntry.append(command.args.ifBlank { "" }, ComponentBuilder.FormatRetention.NONE).color(ChatColor.AQUA)
+            helpEntry.append(" ${command.args}".ifBlank { "" }, ComponentBuilder.FormatRetention.NONE).color(ChatColor.AQUA)
                 .append(" - ").color(ChatColor.DARK_GRAY)
                 .append(command.description).color(ChatColor.GRAY)
 