bd5a1b68cee29e2553c7ffd74af1c5cecc8f5b31
kyleseven <kyle.senerpida@icloud.com>
2020-08-21 00:05:37 -0500

    Implement page buttons.

--- src/main/kotlin/me/kyleseven/consolereader/ui/Page.kt
+++ src/main/kotlin/me/kyleseven/consolereader/ui/Page.kt
@@ -3,7 +3,9 @@ package me.kyleseven.consolereader.ui
 import me.kyleseven.consolereader.config.MainConfig
 import me.kyleseven.consolereader.utils.sendColorMsg
 import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.chat.ClickEvent
 import net.md_5.bungee.api.chat.ComponentBuilder
+import net.md_5.bungee.api.chat.HoverEvent
 import org.bukkit.command.CommandSender
 
 data class Page(
@@ -28,9 +30,38 @@ fun CommandSender.sendPage(page: Page) {
         .append(page.title).color(ChatColor.DARK_AQUA)
         .append(" ======------").color(ChatColor.DARK_GRAY)
 
-    val footer = ComponentBuilder("------====== ").color(ChatColor.DARK_GRAY)
-        .append("${page.pageNumber} of ${page.maxPageNumber}").color(ChatColor.AQUA)
-        .append(" ======------").color(ChatColor.DARK_GRAY)
+    val footer = ComponentBuilder("------====").color(ChatColor.DARK_GRAY)
+
+    if (page.pageNumber == 1) {
+        footer.append(" « ").color(ChatColor.DARK_GRAY)
+    } else {
+        footer.append(" « ").color(ChatColor.GRAY)
+            .event(ClickEvent(ClickEvent.Action.RUN_COMMAND, page.prevCmd))
+            .event(
+                HoverEvent(
+                    HoverEvent.Action.SHOW_TEXT,
+                    ComponentBuilder("Previous Page").color(ChatColor.GRAY).create()
+                )
+            )
+    }
+
+    footer.append("${page.pageNumber} of ${page.maxPageNumber}", ComponentBuilder.FormatRetention.NONE)
+        .color(ChatColor.AQUA)
+
+    if (page.pageNumber == page.maxPageNumber) {
+        footer.append(" » ", ComponentBuilder.FormatRetention.NONE).color(ChatColor.DARK_GRAY)
+    } else {
+        footer.append(" » ", ComponentBuilder.FormatRetention.NONE).color(ChatColor.GRAY)
+            .event(ClickEvent(ClickEvent.Action.RUN_COMMAND, page.nextCmd))
+            .event(
+                HoverEvent(
+                    HoverEvent.Action.SHOW_TEXT,
+                    ComponentBuilder("Next Page").color(ChatColor.GRAY).create()
+                )
+            )
+    }
+
+    footer.append("====------", ComponentBuilder.FormatRetention.NONE).color(ChatColor.DARK_GRAY)
 
     spigot().sendMessage(*header.create())
     for (item in page.content) {
