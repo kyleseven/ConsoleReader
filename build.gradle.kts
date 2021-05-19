9c40862eb2b72e234b92b93993156b1c77a7a258
kyleseven <kyle.senerpida@icloud.com>
2021-05-19 14:36:57 -0500

    Upgrade to Kotlin 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
