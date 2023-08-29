80cd48ab5a8f452cb0f5adea861e913a4d22339c
kyleseven <46883293+kyleseven@users.noreply.github.com>
2023-08-29 15:36:38 -0500

    Upgrade to Gradle 8.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "me.kyleseven"
